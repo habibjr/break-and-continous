9cc8350c6087558bda5198d270fb9239f8b5c617
habibjr <56871584+habibjr@users.noreply.github.com>
2019-10-29 20:16:37 +0700

    Add files via upload

---
+++ BreakAndContinue.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgbreak.and.pkgcontinue;
+
+/**
+ *
+ * @author user
+ */
+public class BreakAndContinue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 1;
+while (i < 10) {
+  System.out.println(i);
+  i++;
+  if (i == 7) {
+    break;
+    }
+    
+}
